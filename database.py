c5efdb51df05302f41cb91c762ff5cf20de0e807
jrsaaguiar <142040043+jrsaaguiar@users.noreply.github.com>
2026-07-16 17:43:10 -0300

    Refactor database.py for improved readability

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, Float, Date, Boolean
+from sqlalchemy import create_engine, Column, Integer, String, Float, Date, Boolean # <- Boolean aqui
 from sqlalchemy.orm import sessionmaker, declarative_base
 import streamlit as st
 
@@ -11,14 +11,14 @@ Base = declarative_base()
 class PosicaoDiaria(Base):
     __tablename__ = "posicoes_diarias"
     id = Column(Integer, primary_key=True, index=True)
-    data = Column(String)  
+    data = Column(String)
     empresa = Column(String)
     tipo_titulo = Column(String)
     valor = Column(Float)
     qtd_veiculos = Column(Integer, default=0)
     valor_medio = Column(Float, default=0.0)
 
-class Usuarios(Base):
+class Usuarios(Base): # <- Classe SÓ aqui
     __tablename__ = 'usuarios'
     id = Column(Integer, primary_key=True)
     email = Column(String(100), unique=True, nullable=False)
@@ -30,4 +30,3 @@ def init_db():
     Base.metadata.create_all(bind=engine)
 
 init_db()
-init_db() # Cria a tabela assim que o arquivo é importado
